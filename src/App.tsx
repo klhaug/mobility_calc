f417c8502acbcf91e355ddc04584be451bbe9439
klhaug <kristian@snougle.no>
2025-08-28 11:06:40 +0200

    average age form working

--- src/App.tsx
+++ src/App.tsx
@@ -40,43 +40,48 @@ function App() {
         />
         </section>
         <section>
-          <fieldset>
-          <legend>Gjennomsnittlig levetid per telefon:</legend>
-            <label 
-              htmlFor="18">
-                18 mnd
-            </label>
-              <input 
-                className="hidden" 
-                type="radio" 
-                id='18' 
-                name="age" 
-                value={18} 
-                onClick={() => console.log("I've been clicked")} />
-            <label 
-              htmlFor="24">
-                24 mnd
-            </label>
-              <input 
-                className="hidden" 
-                type="radio" 
-                id='24' 
-                name="age" 
-                value={24} 
-                />
-            <label 
-              htmlFor="32">
-                32 mnd
-            </label>
-              <input 
-                className="hidden" 
-                type="radio" 
-                id='32' 
-                name="age" 
-                value={32} 
-                />
+          <fieldset className="flex flex-col gap-4 mt-12">
+          <legend className="text-center mb-4 ">Gjennomsnittlig levetid per telefon:</legend>
+                <input
+                  className="hidden peer/18"
+                  type="radio"
+                  id='18'
+                  name="age"
+                  value={18}
+                  onChange={(e) => setAge(e.target.value)}  />
+                   <label
+                className="peer-checked/18:bg-green-800 h-[56px] peer-checked/18:text-white flex  w-full items-center justify-center bg-green-100 hover:cursor-pointer"
+                htmlFor="18">
+                  18 mnd
+                </label>
+                <input
+                  className="hidden peer/24"
+                  type="radio"
+                  id='24'
+                  name="age"
+                  value={24}
+                  onChange={(e) => setAge(e.target.value)}
+                  />
+                   <label
+                    htmlFor="24"
+                    className="peer-checked/24:bg-green-800 peer-checked/24:text-white h-[56px] flex w-full items-center justify-center bg-green-100 hover:cursor-pointer " >
+                  24 mnd
+                </label>
+                <input
+                  className="hidden peer/32"
+                  type="radio"
+                  id='32'
+                  name="age"
+                  value={32}
+                  onChange={(e) => setAge(e.target.value)}
+                  />
+                   <label
+                    htmlFor="32"
+                    className="peer-checked/32:bg-green-800 peer-checked/32:text-white h-[56px] flex  w-full items-center justify-center bg-green-100 hover:cursor-pointer" >
+                  32 mnd
+                  </label>
           </fieldset>
-           
+           {/* <span>{age}</span> */}
         </section>
       </main>
     </>
